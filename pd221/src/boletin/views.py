3f7a269ec4347b97ce60c3787dd2e06c4877a5a9
Ruben Correro <rumonre18@gmail.com>
2019-05-22 22:17:57 +0200

    19.ModelForm en la vista
    Sustituimos el ModelForm para poder guardar objetos nuevos usando nuestro modelo.(RegModelForm)

--- pd221/src/boletin/views.py
+++ pd221/src/boletin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import RegForm
+from .forms import RegForm, RegModelForm
 from .models import Registrado
 
 # Create your views here.
@@ -8,15 +8,32 @@ def inicio(request):
 	titulo = "HOLA"
 	if request.user.is_authenticated:
 		titulo = "Bienvenido  %s" %(request.user)
-	form = RegForm(request.POST or None)
-	if form.is_valid():
-		form_data = form.cleaned_data
-		abc = form_data.get("email")
-		abc2 = form_data.get("nombre")
-		obj = Registrado.objects.create(email=abc, nombre=abc2)
+	form = RegModelForm(request.POST or None)
 
 	context = {
-		"titulo": titulo,
-		"el_form": form,
-	}
+				"titulo": titulo,
+				"el_form": form,
+			}
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		if not instance.nombre:
+			instance.nombre = "PERSONA"
+		instance.save()
+
+		context = {
+			"titulo": "Gracias %s!" %(instance.nombre)
+		}
+		if not (instance.nombre):
+			context = {
+				"titulo": "Gracias %s!" %(email)
+			}
+		print (instance)
+		print (instance.timestamp)
+		#form_data = form.cleaned_data
+		#abc = form_data.get("email")
+		#abc2 = form_data.get("nombre")
+		#obj = Registrado.objects.create(email=abc, nombre=abc2)
+
+
 	return render(request, "inicio.html", context)
