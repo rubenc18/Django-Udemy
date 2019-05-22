449738aa08216a67d09ba5cc94cac4f53f0ec3d8
Ruben Correro <rumonre18@gmail.com>
2019-05-22 16:34:27 +0200

    8.Personalizar modelo en el Admin
    Personalizamos el display en admin.py, algunos campos como nombre, email o timestamp

--- pd221/src/boletin/admin.py
+++ pd221/src/boletin/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Registrado
 
-admin.site.register(Registrado)
+class AdminRegistrado(admin.ModelAdmin):
+	list_display = ["email", "nombre", "timestamp"]
+	#list_display_links = ["nombre"]
+	list_filter = ["timestamp"]
+	list_editable = ["nombre"]
+	search_fields =["email", "nombre"]
+	class Meta:
+		model = Registrado
+
+admin.site.register(Registrado, AdminRegistrado)
