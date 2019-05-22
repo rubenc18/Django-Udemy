25b2f0c324f4eb996c45c5e01d8f8ed52c09927d
Ruben Correro <rumonre18@gmail.com>
2019-05-22 20:52:52 +0200

    17.Validaciones Model Form
    Aquí hemos conseguido crear nuestras propias validaciones.

--- pd221/src/boletin/forms.py
+++ pd221/src/boletin/forms.py
@@ -8,6 +8,14 @@ class RegModelForm(forms.ModelForm):
 		model = Registrado
 		fields = ["nombre", "email"]
 
+	def clean_email(self):
+		email =  self.cleaned_data.get("email")
+		email_base, proveeder = email.split("@")
+		dominio, extension = proveeder.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Por favor utiliza un email con la extenxión .EDU")
+		return email
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
 	email = forms.EmailField()
