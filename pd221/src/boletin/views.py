6e578a83c2e6444a7200999b0115b2125ce43539
Ruben Correro <rumonre18@gmail.com>
2019-05-28 23:29:14 +0200

    38. Contenido para Usuarios Autenticados
    Modificar el contenido para usuarios autenticados

--- pd221/src/boletin/views.py
+++ pd221/src/boletin/views.py
@@ -38,7 +38,10 @@ def inicio(request):
 		#abc2 = form_data.get("nombre")
 		#obj = Registrado.objects.create(email=abc, nombre=abc2)
 
-
+		if request.user.is_authenticated() and request.user.is_staff:
+			context = {
+			"queryset": ['abc', '123'],
+			}
 	return render(request, "inicio.html", context)
 
 def contact(request):
